8394a25f7b15e3210340ad17d7f11e7828eb7fac
Cody Condon <codycondon50@gmail.com>
2016-03-15 14:26:45 -0400

    Add post crud ability

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post, notice: "Post created! The article was successfully saved"
+    else
+      render 'new', notice: "Oh no, Unable to save the post"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "Your article was sucessfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content,)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
